672646f228c7e771f3b2f6236e225260e73818b1
namnguyenvu1 <126105726+namnguyenvu1@users.noreply.github.com>
2023-08-30 22:10:46 -0600

    Create a model
    Create a Linear model

--- main.py
+++ main.py
@@ -1,16 +1,145 @@
-# This is a sample Python script.
+import torch
+from torch import nn
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+print(torch.__version__)
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+# Create *known* parameters
+
+weight = 0.7
+bias = 0.3
+
+# Create
+start = 0
+end = 1
+step = 0.02
+X = torch.arange(start, end, step).unsqueeze(dim=1)
+y = weight * X + bias
+
+# Create a train/test split
+train_split = int(0.8 * len(X))
+X_train, y_train = X[:train_split], y[:train_split]
+X_test, y_test = X[train_split:], y[train_split:]
+
+len(X_train), len(y_train), len(X_test), len(y_test)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def plot_predictions(train_data=X_train,
+                     train_labels=y_train,
+                     test_data=X_test,
+                     test_labels=y_test,
+                     predictions = None):
+  """
+  Plots training data, test data and compares predictions.
+  """
+  plt.figure(figsize=(10,7))
+
+  # Plot training data in blue
+  plt.scatter(train_data, train_labels, c="b", s=4, label="Training data")
+
+  # Plot test data in green
+  plt.scatter(test_data, test_labels, c="g", s=4, label="Testing data")
+
+  # Are there predictions?
+  if predictions is not None:
+    # Plot the predictions if they exist
+    plt.scatter(test_data, predictions, c="r", s=4, label="Predictions")
+
+  # Show the legend
+  plt.legend(prop={"size": 14});
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# Create a Linear Regression model class
+class LinearRegressionModel(nn.Module): # <- almost everything in PyTorch is a nn.Module (think of this as neural network lego blocks)
+    def __init__(self):
+        super().__init__()
+        self.weights = nn.Parameter(torch.randn(1, # <- start with random weights (this will get adjusted as the model learns)
+                                                dtype=torch.float), # <- PyTorch loves float32 by default
+                                   requires_grad=True) # <- can we update this value with gradient descent?)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+        self.bias = nn.Parameter(torch.randn(1, # <- start with random bias (this will get adjusted as the model learns)
+                                            dtype=torch.float), # <- PyTorch loves float32 by default
+                                requires_grad=True) # <- can we update this value with gradient descent?))
+
+    # Forward defines the computation in the model
+    def forward(self, x: torch.Tensor) -> torch.Tensor: # <- "x" is the input data (e.g. training/testing features)
+        return self.weights * x + self.bias # <- this is the linear regression formula (y = m*x + b)
+
+# Create an instance of the model (this is the subclass of nn.Module)
+model_0 = LinearRegressionModel()
+
+# Check out the parameters
+list(model_0.parameters())
+
+# Setup a loss function
+loss_fn = nn.L1Loss()
+
+# Setup an optimizer (stochastic gradient descent)
+optimizer = torch.optim.SGD(params=model_0.parameters(),
+                            lr=0.01) # lr = learning rate = possibly the most important hyperparameter you can set
+
+
+torch.manual_seed(42)
+
+# An epoch is one loop through the data
+epochs = 100
+
+# Track different values
+epoch_count = []
+loss_values = []
+test_loss_values = []
+
+### Training
+# 0. Loop through the data
+for epoch in range (epochs):
+  # Set the model to training mode
+  model_0.train() # Train mode in PyTorch sets all parameters that require gradients to require gradients
+
+  # 1. Forward pass
+  y_pred = model_0(X_train)
+
+  # 2. Calculate the loss
+  loss = loss_fn(y_pred, y_train) # output = loss(input, target)
+
+  # 3. Optimizer zero grad
+  optimizer.zero_grad()
+
+  # 4. Perform backpropagation on the loss with respect to the parameters of the model
+  loss.backward()
+
+  # 5. Step the optimizer (perform gradient descent)
+  optimizer.step()
+
+  ### Testing
+  model_0.eval() # turn off gradient tracking
+  with torch.inference_mode(): # turns off gradient tracking
+    # 1. Do the forward pass
+    test_pred = model_0(X_test)
+
+    # 2. Calculate the loss
+    test_loss = loss_fn(test_pred, y_test)
+
+  # Print out what's happenin'
+  if epoch % 10 == 0:
+    epoch_count.append(epoch)
+    loss_values.append(loss)
+    test_loss_values.append(test_loss)
+    print(f"Epoch: {epoch} | Loss: {loss} | Test loss: {test_loss}")
+    # Print out model state_dict()
+    print(model_0.state_dict())
+
+
+# Plot the loss curve
+#plt.plot(epoch_count, loss_values, label="Train loss")
+plt.plot(epoch_count, np.array(torch.tensor(loss_values).cpu().numpy()), label="Train loss")
+plt.plot(epoch_count, test_loss_values, label="Test loss")
+plt.title("Training and test loss curves")
+plt.ylabel("Loss")
+plt.xlabel("Epochs")
+plt.legend();
+
+with torch.inference_mode():
+  y_preds_new = model_0(X_test)
+
+plot_predictions(predictions=y_preds_new);
